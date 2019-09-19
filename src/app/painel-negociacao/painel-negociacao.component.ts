d2971b19278ed8522856b287d7e62189b73f6a10
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 15:29:45 -0300

    Open house

---
+++ src/app/painel-negociacao/painel-negociacao.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { OportunidadeService } from '../oportunidade.service';
+
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-painel-negociacao',
+  templateUrl: './painel-negociacao.component.html',
+  styleUrls: ['./painel-negociacao.component.css']
+})
+export class PainelNegociacaoComponent implements OnInit {
+
+
+
+  constructor(
+
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+
+
+}
